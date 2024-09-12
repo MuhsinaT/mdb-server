2b9b7abcb21269d4a7c698d0df438037f285a217
Muhsina parveen <muhsinamuhsina703@gmail.com>
2024-09-12 20:07:34 +0530

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer=require('json-server')
+
+const mdbserver=jsonServer.create()
+const middleware=jsonServer.defaults()
+const router=jsonServer.router('db.json')
+
+mdbserver.use(middleware)
+mdbserver.use(router)
+
+const PORT=3000 || process.env.PORT
+
+mdbserver.listen(PORT,()=>{
+    console.log("Server Running at :",PORT);
+    
+})
